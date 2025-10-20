b44ddb892390ea4e1fea3c15bc8d3757136a730d
tomatosara <Sarahuang997@gmail.com>
2025-10-20 14:45:18 +0800

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>零知識防詐交易平台</h1>
+      <p>這是我使用 Rolldown 啟動的第一個前端專案 🚀</p>
+      <button
+        style={{
+          padding: "10px 20px",
+          fontSize: "16px",
+          backgroundColor: "#10B981",
+          color: "white",
+          borderRadius: "8px",
+          border: "none",
+        }}
+      >
+        開始驗證
+      </button>
+    </div>
+  );
+}
+
+export default App;
